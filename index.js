1b1a74be16f46296401eb575c918a868657bda2d
Mithilesh Tarkar <mithilesh.tarkar@ves.ac.in>
2018-07-03 16:15:57 +0530

    removed access control

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 
-router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://learnindia.herokuapp.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-
 app.get('/',(req,res)=>{
 
         res.send('Hello from Backend');
