349ff32f45da7cacb50791367636044ad5986298
Mithilesh Tarkar <mithilesh.tarkar@ves.ac.in>
2018-07-04 11:38:23 +0530

    removed custom cors function

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 //allow-cors
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  })
+// app.use(function(req,res,next){
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   })
 app.get('/',(req,res)=>{
 
         res.send('Hello from Backend');
